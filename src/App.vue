3c2041537ab997879953d4074c19755eb5b75cc6
lihengda <522361856@qq.com>
2020-05-12 14:28:40 +0800

    5-12

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="app">
-    <name-avatar scale='1' name="李亨达"></name-avatar>
+  <div id="app" class="center">
+    <div class="mr" >
+      <name-avatar scale='2' name="李亨达"></name-avatar>
+    </div>
+    <div>
+      <name-avatar scale='2' faceUrl="https://c-ssl.duitang.com/uploads/item/201809/21/20180921212643_omxtr.jpg"></name-avatar>
+    </div>
   </div>
 </template>
 <script>
@@ -16,4 +21,14 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.center {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .mr{
+    margin: 59px;
+  }
+}
 </style>
